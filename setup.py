4182afc94c850f0c810296644036f9fa93d77594
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-16 15:42:01 +0200

    Prepare python project

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logger",
+    version="0.0.2",
+    author="Ruben Lipperts",
+    author_email="",
+    description="Simple logger for simultaneous file and shell logging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rlipperts/logger",
+    packages=setuptools.find_packages(include='logger'),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Benchmark",
+    ],
+    python_requires='~=3.9',
+)
