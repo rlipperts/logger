b18a73c4dc47f653fd1fc7112af75932a9600f49
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-16 15:56:47 +0200

    Rename this to avoid conflicts

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="logger",
+    name="loggin-configurator",
     version="0.0.2",
     author="Ruben Lipperts",
     author_email="",
-    description="Simple logger for simultaneous file and shell logging",
+    description="Simple logger configuration for simultaneous file and shell logging",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rlipperts/logger",
-    packages=setuptools.find_packages(include='logger'),
+    packages=setuptools.find_packages(include='logging_configurator'),
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
