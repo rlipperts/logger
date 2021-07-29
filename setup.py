30fdc636546e4f65f2399a4c042a04079ab809b0
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-29 14:15:36 +0200

    Mark package as typed for mypy

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 test_deps = [
@@ -14,24 +14,24 @@ extras = {
 }
 
 setuptools.setup(
-    name="logging-configurator",
-    version="0.0.2",
-    author="Ruben Lipperts",
-    author_email="",
-    description="Simple logger configuration for simultaneous file and shell logging",
+    name='logging-configurator',
+    version='0.0.2',
+    author='Ruben Lipperts',
+    author_email='',
+    description='Simple logger configuration for simultaneous file and shell logging',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rlipperts/logger",
+    long_description_content_type='text/markdown',
+    url='https://github.com/rlipperts/logger',
     package_dir={'': 'src'},
     packages=['logging_configurator'],
     package_data={'logging_configurator': ['py.typed']},
     tests_require=test_deps,
     extras_require=extras,
     classifiers=[
-        "Programming Language :: Python :: 3.9",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Logging",
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: System :: Logging',
     ],
     python_requires='~=3.9',
 )
