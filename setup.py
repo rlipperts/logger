004576c659d04f2df5af8b2f506d26601589724b
rlipperts <ruben.lipperts@uni-bielefeld.de>
2021-07-29 13:56:57 +0200

    Include 'test' extra in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+test_deps = [
+    'pytest',
+    'flake8',
+    'pylint',
+    'mypy',
+]
+extras = {
+    'test': test_deps
+}
+
 setuptools.setup(
     name="logging-configurator",
     version="0.0.2",
@@ -12,7 +22,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rlipperts/logger",
-    packages=["logging_configurator"],
+    package_dir={'': 'src'},
+    packages=['logging_configurator'],
+    package_data={'logging_configurator': ['py.typed']},
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
